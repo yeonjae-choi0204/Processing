110d71ae74c1bbccbf73b06a9dda1049308b00bc
yeonjae-choi0204 <70361363+yeonjae-choi0204@users.noreply.github.com>
2020-09-11 13:38:27 +0900

    아무거나

---
+++ sketch_200904b/sketch_200904b.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(500, 500);
+  point(250, 250);
+  line(0, 0, 500, 500);
+  ellipse(250, 250, 300, 300);
+  triangle(250, 100, 100, 250, 400, 250);
+ }
+
+
+function draw() {
+
+}
