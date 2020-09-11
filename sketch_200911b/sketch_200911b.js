16c00b67372a62af3d650b17dc1caa520a21e358
yeonjae-choi0204 <70361363+yeonjae-choi0204@users.noreply.github.com>
2020-09-11 15:01:50 +0900

    dkarjsk

---
+++ sketch_200911b/sketch_200911b.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(500, 500);
+  
+  fill(255, 0, 0);
+  ellipse(100, 100, 50, 50);
+  fill(232, 247, 25);
+  ellipse(200, 100, 100, 100);
+  fill(213, 172, 247);
+  ellipse(300, 300, 200, 200);
+  fill(147, 209, 255);
+  ellipse(400, 450, 10, 10);
+  fill(255, 5, 226);
+  ellipse(400, 400, 60, 60);
+
+}
+
+
+function draw() {
+
+}
